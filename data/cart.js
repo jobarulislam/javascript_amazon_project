a02e6f1570e8016b6b1cc2cc07d15447720e48f7
Md Jobarul Islam <jobarulislam1203@gmail.com>
2026-02-19 02:16:11 +0600

    Enhance cart structure with delivery options
    Added deliveryOptionsId to cart items and updated related logic.

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,30 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
+
+/*
+    IIFE function syntax
+(function())()
+(()=> {})()
+
+
+(function(){
+    let sum = 0;
+    cart.length > 0 ? cart.forEach(item => {
+        sum+=item.quantity;
+    }) : 0
+    document.getElementById("nav-cart-count").innerHTML = sum;
+})()
+    */
 if (!cart){
 cart = [{
     productId:'3ebe75dc-64d2-4137-8860-1f5a963e534b',
-    quantity : 2
+    quantity : 2,
+    deliveryOptionsId: '1'
 
 },
 {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
+    quantity:1,
+    deliveryOptionsId: '2'
 }];
 }
 function saveToStorage(){
@@ -26,10 +43,21 @@ export function addToCart(productId) {
     else {
         cart.push({
             productId: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionsId: '1'
         });
     }
 
+    saveToStorage();
+}
+ export function updateDeliveryOption(productId, deliveryOptionId ){
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+    matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
 
